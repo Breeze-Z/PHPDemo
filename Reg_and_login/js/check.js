01b7a3502722393d59da919854cccad62ac0c3a5
weisen.zhang <weisen.zhang@PC-012.pcase.com.cn>
2015-11-04 10:04:39 +0800

    完成前台js数据检测

--- Reg_and_login/js/check.js
+++ Reg_and_login/js/check.js
@@ -1,53 +1,29 @@
 console.log('前台防止恶意注册');
-/*function checkUser(str){
-    var re = /^[\w\x80-\xff]{3,15}$/;
-    if(!re.test(str)){
-        alert('用户名不规范，请重新输入');
-        return false;
-    }else{
-        return true;
-    }
-}
-
-function check_password(str){
-    var re=str.length;
-    if(re<6){
-        alert('密码长度要大于6位数');
-        return false;
-    }else{
-        return true;
-    }
-}
-
-function check_repass(pass,repass){
-    if(pass!=repass){
-        alert('密码不一致，请重新输入');
-        return false;
-    }else{
-        return true;
-    }
-}
-
-function check_email(str){
-    var re=/^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
-    if(!re.test(str)){
-        alert('邮箱格式不规范，请重新输入');
-        return false;
-    }else{
-        return true;
-    }
-}
-*/
 window.onload = function(){
 	var fm = document.getElementsByTagName('form')[0];
-	fm.onsubmit=function(){
-	var username = fm.username.value;
-            if(username.length <2 || username.length > 20){
-                    alert("用户名不得小于2位或不能大于20位");
-                    fm.username.value = "";
-                    fm.username.focus();
+	fm.onsubmit = function(){
+            var username = fm.username.value;          
+                if(!/^[\w\x80-\xff]{3,15}$/.test(username)){
+                     alert('用户名不规范，请重新输入');
+                     return false;
+                }
+            var password = fm.password.value;
+                var re=password.length;
+                if(re<6){
+                    alert('密码长度要大于6位数');
                     return false;
-		}
+                }
+            var repass = fm.regpass.value;
+                if(password!=repass){
+                    alert('密码不一致，请重新输入');
+                    return false;
+                 }           
+            var email = fm.email.value;
+                if(!/^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/.test(email)){
+                    alert('邮箱格式不规范，请重新输入');
+                    return false;
+                }
+                
 	}
 }
 
